e52d687c163755c7f4c91184ba4b01ad5a52731f
Dmitrii Shatokhin <dmtrshatokhin@gmail.com>
2024-12-23 13:37:59 +0100

    ci: refactor get_versions to use AppState for version management and retrieve values from Secrets

--- gcsst-ui/src/main.rs
+++ gcsst-ui/src/main.rs
@@ -1,9 +1,7 @@
 use actix_web::{web, HttpResponse, Responder, Result};
-use dotenv::dotenv;
 use gcsst_lib::transmute_from_content;
 use serde::{Deserialize, Serialize};
 use shuttle_actix_web::ShuttleActixWeb;
-use std::env;
 
 #[derive(Deserialize)]
 struct CssInput {
@@ -22,13 +20,16 @@ struct VersionsResponse {
     gcsst_ui_version: String,
 }
 
-async fn get_versions() -> impl Responder {
-    let gcsst_version = env::var("GCSST_VERSION").unwrap_or_else(|_| "unknown".to_string());
-    let gcsst_ui_version = env::var("GCSST_UI_VERSION").unwrap_or_else(|_| "unknown".to_string());
+#[derive(Clone)]
+struct AppState {
+    gcsst_version: String,
+    gcsst_ui_version: String,
+}
 
+async fn get_versions(data: web::Data<AppState>) -> impl Responder {
     HttpResponse::Ok().json(VersionsResponse {
-        gcsst_version,
-        gcsst_ui_version,
+        gcsst_version: data.gcsst_version.clone(),
+        gcsst_ui_version: data.gcsst_ui_version.clone(),
     })
 }
 
@@ -55,11 +56,23 @@ async fn transmute(input: web::Json<CssInput>) -> impl Responder {
 
 #[shuttle_runtime::main]
 async fn shuttle_main(
+    #[shuttle_runtime::Secrets] secrets: shuttle_runtime::SecretStore,
 ) -> ShuttleActixWeb<impl FnOnce(&mut web::ServiceConfig) + Send + Clone + 'static> {
-    dotenv().ok();
+    let gcsst_version = secrets
+        .get("GCSST_VERSION")
+        .unwrap_or("unknown".to_string());
+    let gcsst_ui_version = secrets
+        .get("GCSST_UI_VERSION")
+        .unwrap_or("unknown".to_string());
+
+    let app_state = web::Data::new(AppState {
+        gcsst_version,
+        gcsst_ui_version,
+    });
 
     let config = move |cfg: &mut web::ServiceConfig| {
-        cfg.service(web::resource("/").route(web::get().to(render_index)))
+        cfg.app_data(app_state.clone())
+            .service(web::resource("/").route(web::get().to(render_index)))
             .service(web::resource("/transmute").route(web::post().to(transmute)))
             .service(web::resource("/versions").route(web::get().to(get_versions)))
             .service(actix_files::Files::new("/static", "./static").index_file("index.html"));
